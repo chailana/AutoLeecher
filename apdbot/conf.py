148962cc5993cae3d306128453074d25ff5963d1
chailana <chailana35217793@gmail.com>
2024-09-12 13:10:05 +0530

    Update conf.py

--- apdbot/conf.py
+++ apdbot/conf.py
@@ -2,8 +2,8 @@ from apdbot.sample_con import Config
 #Fill your all data here Do not Edit other Files
 
 class Config(Config):
-    BOT_TOKEN= ""
-    APP_ID = 
-    API_HASH = ""
-    OWNER_ID = ""
-    AUTH = [556315181, -100088888, -10002252254]
+    BOT_TOKEN= "7513058089:AAHAPtJbHEPbRMbV8rv-gAZ8KVL0ykAM2pE"
+    APP_ID = 22420997
+    API_HASH = "d7fbe2036e9ed2a1468fad5a5584a255"
+    OWNER_ID = "7170426058"
+    AUTH = [-1002481981021]
